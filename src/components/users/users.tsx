d86df8f10e11d2a43f7f094945ada8e0d00f236d
Cate Uch <cat.uch@yandex.ru>
2021-03-05 00:51:23 +0300

    add componentDidMount, life cicle methods

--- src/components/users/users.tsx
+++ src/components/users/users.tsx
@@ -3,36 +3,35 @@ import axios from 'axios';
 import React from 'react';
 import userPhoto from './../pics/small.jpg';
 
-class Users extends React.Component  {
+class Users extends React.Component {
 
-	getUsers = () => {
+	componentDidMount() {
 		axios.get(`https://social-network.samuraijs.com/api/1.0/users`).then(response => { this.props.setUsers(response.data.items) }
-		);
+		)
 	}
 
-    render () {
-        return <div>
-			<button onClick={this.getUsers}></button>
-		{
-			this.props.users.map(u => <div key={u.id}>
-				<div>
-				<img  src={u.photos.small ? u.photos.small : userPhoto} />
+	render() {
+		return <div>
+			{
+				this.props.users.map(u => <div key={u.id}>
+					<div>
+						<img src={u.photos.small ? u.photos.small : userPhoto} />
+					</div>
+					<div>
+
+						<div>{u.name}</div><p />
+						<div>{u.status ? u.status : "hi, everyone!"}</div>
+						<div>{"u.localcity"}</div>
+						<div>{"u.localcountry"}</div>
+					</div>
+					<div>{u.follow
+						? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+						: <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
+					}
+					</div>
 				</div>
-				<div>
-				
-					<div>{u.name}</div><p/>
-					<div>{u.status ? u.status : "hi, everyone!"}</div>
-					<div>{"u.localcity"}</div>
-					<div>{"u.localcountry"}</div>
-				</div>
-				<div>{u.follow
-					? <button onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-					: <button onClick={() => { this.props.follow(u.id) }}>Follow</button>
-				}
-				</div>
-			</div>
-			)}
-	</div>
-    }
+				)}
+		</div>
+	}
 }
 export default Users;
